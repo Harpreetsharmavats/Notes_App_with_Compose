db16b0cee3e3f19ebcc78a19c37d7175a51a7d15
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-02-15 17:19:24 +0530

    changes

--- app/src/main/java/com/example/notesappwithcompose/Presentation/NoteScreen.kt
+++ app/src/main/java/com/example/notesappwithcompose/Presentation/NoteScreen.kt
@@ -1,12 +1,23 @@
 package com.example.notesappwithcompose.Presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -15,9 +26,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.notesappwithcompose.ui.theme.Pink80
 
@@ -46,15 +59,72 @@ fun NoteScreen(
                 )
                 IconButton(onClick = { onEvent(NotesEvent.SortNotes) }) {
                     Icon(
-                        imageVector = Icons.Rounded.Sort, contentDescription = null,
+                        imageVector = Icons.Rounded.List, contentDescription = null,
                         modifier = Modifier.size(35.dp),
                         tint = Pink80
                     )
                 }
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(containerColor = Pink80, onClick = {
+                state.title.value = ""
+                state.description.value = ""
+                navController.navigate("AddNoteScreen")
+            }) {
+                Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
+
+            }
         }
 
     ) {
+        LazyColumn(
+            contentPadding = it,
+
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(state.notes.size) {
+                NoteItem(
+                    state = state,
+                    index = it,
+                    onEvent = onEvent
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteItem(state: NoteState, index: Int, onEvent: (NotesEvent) -> Unit) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(Pink80)
+            .padding(12.dp)
+    ) {
+        Column(Modifier.weight(1f)) {
+            Text(
+                text = state.notes.get(index = index).title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = state.notes.get(index = index).description,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.Delete,
+            contentDescription = null,
+            modifier = Modifier.size(35.dp)
+        )
 
     }
 }
